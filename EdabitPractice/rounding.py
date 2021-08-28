ce3cae21cabf8865553fcf3c7b1c0f9120b2ef13
Darren Rodricks <86697301+Darrenrodricks@users.noreply.github.com>
2021-08-28 16:33:31 -0700

    Add files via upload

---
+++ EdabitPractice/rounding.py
@@ -0,0 +1,5 @@
+# Write a Python program to round a floating-point number to specified number decimal places.
+order_amt = 212.374
+print('\nThe total order amount comes to {}'.format(order_amt))
+print('The total order amount comes to {}'.format(round(order_amt, 2)))
+print()

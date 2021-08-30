d8b208a75bd989bb658693eeefa3b8a2b494afd9
Darren Rodricks <86697301+Darrenrodricks@users.noreply.github.com>
2021-08-30 12:55:53 -0700

    Add files via upload

---
+++ EdabitPractice/returnSomethingtoMe.py
@@ -0,0 +1,7 @@
+# Return Something to Me!
+# Write a function that returns the string "something" joined with a space " " and the given argument a.
+def return_me(a):
+    print("something", a)
+
+a = input("PLease enter something: ")
+return_me(a)
